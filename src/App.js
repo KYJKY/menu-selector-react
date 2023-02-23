f5ebb2c50f2647495da51cc2a094883deb219234
김연중Home-PC <illulizer@gmail.com>
2023-02-24 00:34:52 +0900

    이미지 링크 수정, 메인 페이지 CSS 적용 reset.css 적용

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
+import styles from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.wrap}>
       <Header />
-      <main className="box-menu">
-        <button id="menu1" className="menu">
+      <main className={styles['box-menu']}>
+        <button id="menu1" className={styles.menu}>
           <h3>장부식당<br />돌림판</h3>
-          <div><img src="public/images/bet.png" alt="메뉴(돌림판)" className="img-menu" /></div>
+          <div><img src="images/bet.png" alt="메뉴(돌림판)" className={styles['img-menu']} /></div>
         </button>
-        <button id="menu2" className="menu">
+        <button id="menu2" className={styles.menu}>
           <h3>점심메뉴<br />랜덤슬롯</h3>
-          <div><img src="public/images/slot-machine.png" alt="메뉴(룰랫)" className="img-menu" /></div>
+          <div><img src="images/slot-machine.png" alt="메뉴(룰랫)" className={styles['img-menu']} /></div>
         </button>
-        <button id="menu3" className="menu">
+        <button id="menu3" className={styles.menu}>
           <h3>장부식당<br />지도</h3>
-          <div><img src="public/images/location.png" alt="메뉴(지도)" className="img-menu" /></div>
+          <div><img src="images/location.png" alt="메뉴(지도)" className={styles['img-menu']} /></div>
         </button>
-        <button id="menu4" className="menu">
+        <button id="menu4" className={styles.menu}>
           <h3>장부식당<br />메뉴정보</h3>
-          <div><img src="public/images/restaurant.png" alt="메뉴(식당정보)" className="img-menu" /></div>
+          <div><img src="images/restaurant.png" alt="메뉴(식당정보)" className={styles['img-menu']} /></div>
         </button>
       </main>
       <Footer />
--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import styles from './Footer.module.css';
 
 function Footer() {
     return (
-        <footer>
+        <footer className={styles.footer}>
             {/*<img src="" alt="스포츠투아이 로고" />*/}
             <p>문의사항: 개발3팀 김연중 kkk3052@sports2i.com</p>
             <p>Copyright © sports2i Corp</p>
